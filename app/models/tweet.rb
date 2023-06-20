f64c221794d32185efef4263de61da5960ff6c9e
kawasaki-masayuki <purple-wisteria@outlook.jp>
2023-06-20 10:13:25 +0900

    投稿検索機能の実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,12 @@ class Tweet < ApplicationRecord
   validates :text,  presence: true
   belongs_to :user
   has_many :comments  # commentsテーブルとのアソシエーション
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?) OR title LIKE(?)', "%#{search}%", "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
