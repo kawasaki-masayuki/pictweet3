06271e2b349390e645100e6c2923aa863e6da347
kawasaki-masayuki <purple-wisteria@outlook.jp>
2023-07-11 19:48:54 +0900

    再commit

---
+++ spec/models/tweet_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Tweet, type: :model do
+  before do
+    @tweet = FactoryBot.build(:tweet)
+  end
+
+  describe 'ツイートの保存' do
+    context 'ツイートが投稿できる場合' do
+      it 'タイトルと画像とテキストを投稿できる' do
+        expect(@tweet).to be_valid
+      end
+    end
+
+    context 'ツイートが投稿できない場合' do
+      it 'タイトルが空では投稿できない' do
+        @tweet.title = ''
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include('タイトルを入力してください')
+      end     
+      it '画像がないとツイートは保存できない' do
+        @tweet.image = nil
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include('画像を入力してください')
+      end
+      it 'テキストが空では投稿できない' do
+        @tweet.text = ''
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include('テキストを入力してください')
+      end     
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @tweet.user = nil
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include('Userを入力してください')
+      end
+    end
+  end
+end
